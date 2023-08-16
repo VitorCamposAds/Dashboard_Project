042c34f75bc37fac9da96268b9aeb0ab4c2955dc
VitorCamposAds <vitorbeatle@gmail.com>
2023-08-16 05:03:15 -0300

    deploy4

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -71,7 +71,9 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'dashboard.wsgi.application'
+#WSGI_APPLICATION = 'dashboard.wsgi.application'
+WSGI_APPLICATION = 'dj_static:Cling'
+
 
 
 # Database
@@ -133,7 +135,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
-STATIC_ROOT = os.path.join('static')
+#STATIC_ROOT = os.path.join('static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
